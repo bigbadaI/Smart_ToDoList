e8b11cd887afb811e2c991af80f9560b6c9fee69
BigBadAI <flashmefusion@hotmail.ca>
2021-02-05 22:35:56 +0000

    Create db for midterm

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,29 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS favourites CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  description TEXT,
+  complete BOOLEAN DEFAULT false,
+  due_date DATE
+);
+
+CREATE TABLE favourites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE
+);
+
+-- owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
