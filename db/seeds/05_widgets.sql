3880a67cd1b030e39b0a13b6cbb2af2bbde41551
gforsythe <gabe.forsythe@gmail.com>
2021-02-06 18:20:43 +0000

    Adding tasks seeds

--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -1,4 +1,14 @@
 -- Widgets table seeds here (Example)
-INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
-INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
-INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('Harry Potter and the Prisoner of Azkaban', 'Book','Read The Book: Harry Potter', 2021-02-22);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('The Perks of Being a Wallflower', 'Movie', 'Watch The Movie: The Perks of Being a Wallflower', 2021-02-17);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('WOVTE Bear Claw Black Telescopic Back Scratcher', 'Product', 'Buy: WOVTE Bear Claw Black Telescopic Back Scratcher', 2021-02-14);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('Indian Horse', 'Book', 'Read The Book: Indian Horse', 2021-02-14);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('BOSS Digital Delay Guitar Effect Pedal (DD-3T)', 'Product', 'Buy: BOSS Digital Delay Guitar Effect Pedal (DD-3T)', 2021-02-16);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('Autumn', 'Book', 'Read The Book: Autumn', 2021-02-28);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('Men Of Honour', 'Movie', 'Watch The Movie: Men Of Honour', 2021-02-23);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('Skittles', 'Product', 'Buy: Skittles Original, Mega-Pack', 2021-02-19);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('100 Days Of Solitude', 'Book', 'Read The Book: 100 Days Of Solitude', 2021-02-11);
+INSERT INTO tasks (user_id, title, category, description, complete, due_date) VALUES ('FernGully: The Last Rainforest', 'Movie', 'Watch The Movie: FernGully: The Last Rainforest', 2021-02-24);
+
+
+
