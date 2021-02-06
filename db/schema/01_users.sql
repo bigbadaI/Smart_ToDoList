27d05b75bc450a62cf525a3a8709948749dc76c0
BigBadAI <flashmefusion@hotmail.ca>
2021-02-06 03:33:54 +0000

    Small tweeks

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -7,7 +7,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  username VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE tasks (
---
+++ db/seeds/generate-users.js
@@ -0,0 +1,31 @@
+const faker = require('faker');
+const fs = require('fs')
+console.log("HELLO")
+function generateUsers() {
+
+  let users = []
+
+  for (let id=0; id <= 1; id++) {
+
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    let email = faker.internet.email();
+    let username = faker.internet.userName();
+
+    users.push({
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email,
+        "username":username
+    });
+  }
+  // console.log(users);
+  return { "data": users }
+}
+
+let dataObj = generateUsers();
+// generateUsers();
+console.log(dataObj)
+console.log(dataObj)
+// fs.writeFileSync('data.json', JSON.stringify(dataObj, null, '\t'));
+
