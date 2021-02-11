acc1de281b301422b318b4c527ed63d9236e7453
JJ <jerrica.mj@gmail.com>
2021-02-11 00:22:49 +0000

    Add queryMatch function to the helpers file to clean up widgets.js.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,10 +1,19 @@
 
-//checks two arrays to see if they include any matching values
+/**
+ * Compare two arrays to check if they have any identical values.
+ * @param {*} arr1 First array to compare elements.
+ * @param {*} arr2 Second array to compare elements.
+ * @return {Boolean} Return true if there is a matching item in both arrays.
+ */
 const findCommonElements = function (arr1, arr2) {
   return arr1.some(item => arr2.includes(item));
 };
 
+
 //building an object to compare types to categories
+/**
+ * An object to hold all of the categories to be displayed (as keys), and their corresponding types (as an array), as returned from the Google search.
+ */
 let compareObj = {
   'To Watch': ['Movie', 'TVSeries', 'SportsTeam', 'MovieSeries', 'Painting', 'TVEpisode'],
   'To Listen To': ['Compsition', 'MusicGroup', 'BroadcastService', 'RadioSeries', 'RadioStation', 'MusicAlbum', 'MusicRelease', 'Audiobook'],
@@ -14,7 +23,25 @@ let compareObj = {
 };
 
 
+/**
+ * Check the categories object to find a category with a type array value that matches a type of the Google search element.
+ */
+const queryMatch = function (arr, obj, val) {
+  for (let element of arr) {
+    for (let key in obj) {
+      let match = findCommonElements(obj[key], element);
+      if (match) {
+        return val = key;
+      }
+    }
+  }
+};
+
+
+/**
+ *
+ */
 
 
 
-module.exports = { findCommonElements, compareObj };
+module.exports = { findCommonElements, compareObj, queryMatch };
